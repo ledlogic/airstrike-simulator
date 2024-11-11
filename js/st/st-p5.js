0cc5db6a33f6342efdb99dfc9f52e88e20381f94
Jeff Conrad <ledlogic@gmail.com>
2024-11-11 03:15:31 -0600

    Added planes

--- js/st/st-p5.js
+++ js/st/st-p5.js
@@ -3,41 +3,138 @@ function setup() {
 }
 
 function draw() {
-	background(220);
-	
-	stroke(0);
-	strokeWeight(0.5);
+	st.p5.drawSky();
+	st.p5.drawPlanes();
+}
 
-	var z1 = 0;
-	var z2 = 0;
-
-	for (var x = -st.p5.w; x <= st.p5.w; x += st.p5.w / 20) {
-		var x1 = x;
-		var x2 = x;
-		var y1 = -st.p5.h;
-		var y2 = st.p5.h;
-		line(x1, y1, x2, y2);
-	}
-	for (var y = -st.p5.h; y <= st.p5.h; y += st.p5.h / 20) {
-		var y1 = y;
-		var y2 = y;
-		var x1 = -st.p5.w;
-		var x2 = st.p5.w;
-		line(x1, y1, x2, y2);
-	}
+function preload() {
+	st.p5.font = loadFont('./font/inconsolata.otf');
 }
 
 st.p5 = {
-	init() {
+	ratio: 500,
+
+	real: {
+		height: 10000,
+		width: 5000
+	},
+
+	init: function() {
 		var w = window.innerWidth - 32;
 		var h = window.innerHeight - 140;
-		
+
+		st.p5.real.ratio = st.p5.real.height / h;
+
 		var canvas = $("#st-canvas")[0];
 		createCanvas(w, h, WEBGL, canvas);
 		st.p5.w = w;
 		st.p5.h = h;
+
+		st.planes.init();
+	},
+
+	drawSky: function() {
+		var ratio = st.p5.real.ratio;
+
+		stroke(155, 155, 255);
+		strokeWeight(0.5);
+		for (var realx = -5000; realx <= 10000; realx += 500) {
+			var x1 = realx / ratio;
+			var x2 = x1;
+			var y1 = -10000 / ratio;
+			var y2 = 10000 / ratio;
+			line(x1, y1, x2, y2);
+		}
+		for (var realy = -5000; realy <= 10000; realy += 500) {
+			var x1 = -10000 / ratio;
+			var x2 = 10000 / ratio;
+			var y1 = realy / ratio;
+			var y2 = y1;
+			line(x1, y1, x2, y2);
+		}
+
+		strokeWeight(1);
+		for (var realx = -5000; realx <= 10000; realx += 2500) {
+			var x1 = realx / ratio;
+			var x2 = x1;
+			var y1 = -10000 / ratio;
+			var y2 = 10000 / ratio;
+			line(x1, y1, x2, y2);
+		}
+		for (var realy = -5000; realy <= 10000; realy += 2500) {
+			var x1 = -10000 / ratio;
+			var x2 = 10000 / ratio;
+			var y1 = realy / ratio;
+			var y2 = y1;
+			line(x1, y1, x2, y2);
+		}
+
+		strokeWeight(2);
+		var x1 = 0 / ratio;
+		var x2 = 0;
+		var y1 = -10000 / ratio;
+		var y2 = 10000 / ratio;
+		line(x1, y1, x2, y2);
+
+		var x1 = -10000 / ratio;
+		var x2 = 10000 / ratio;
+		var y1 = 0;
+		var y2 = 0;
+		line(x1, y1, x2, y2);
+	},
+
+	drawPlanes: function() {
+		strokeWeight(3);
+
+		var ratio = st.p5.real.ratio;
+		var planes = st.planes.planes;
+		for (var i = 0; i < planes.length; i++) {
+			var plane = planes[i];
+
+			var x = plane.x / ratio;
+			var y = -plane.y / ratio;
+			var a = plane.a;
+			var canvasa = 90.0 - a;
+
+			// fuselage
+			stroke('gray');
+			fill(0, 0, 0, 0);
+			var r = 10;
+			var x1 = x + 0.75 * r * Math.cos(canvasa / 180.0 * Math.PI);
+			var y1 = y - 0.75 * r * Math.sin(canvasa / 180.0 * Math.PI);
+			var x2 = x + r * Math.cos((canvasa + 180.0) / 180.0 * Math.PI);
+			var y2 = y - r * Math.sin((canvasa + 180.0) / 180.0 * Math.PI)
+			line(x1, y1, x2, y2);
+			
+			// wing
+			var winga = canvasa + 90.0;
+			var r = 10.0;
+			var x1 = x + r * Math.cos(winga / 180.0 * Math.PI);
+			var y1 = y - r * Math.sin(winga / 180.0 * Math.PI);
+			var x2 = x + r * Math.cos((winga + 180.0) / 180.0 * Math.PI);
+			var y2 = y - r * Math.sin((winga + 180.0) / 180.0 * Math.PI)
+			line(x1, y1, x2, y2);
+			
+			stroke('gray');
+			var r = 4;
+			var x1 = x + r * Math.cos(canvasa / 180.0 * Math.PI);
+			var y1 = y - r * Math.sin(canvasa / 180.0 * Math.PI);
+			var x2 = x + r * Math.cos((canvasa + 180.0) / 180.0 * Math.PI);
+			var y2 = y - r * Math.sin((canvasa + 180.0) / 180.0 * Math.PI)
+			circle(x1, y1, 2);
 		
-		//var cam = createCamera();
-		//cam.lookAt(0, 0, 0);
+			// light blue center and outer radius
+			stroke(155, 155, 255);
+			circle(x, y, 2);
+			circle(x, y, 25);
+
+			fill('red');
+			textFont(st.p5.font);
+			var t = "p" + i + " (" + Math.round(a) + ")";
+			text(t, x-20, y + 20);
+
+			var t = "(" + Math.round(plane.x) + ", " + Math.round(plane.y) + ")";
+			text(t, x-20, y + 30);
+		}
 	}
 };
---
+++ js/st/st-planes.js
@@ -0,0 +1,27 @@
+/* st-planes.js */
+
+st.planes = {
+	planes: [],
+	init: function() {
+		st.planes.createPlanes("soviet", "mig-29", 10);
+	},
+	createPlanes: function(team, type, qty) {
+		for (var i=0;i<qty;i++) {
+			var x = (Math.random() * st.p5.real.height) - 0.5 * st.p5.real.height;
+			var y = (Math.random() * st.p5.real.width) - 0.5 * st.p5.real.width;
+			var a = (Math.random() * 360);
+			var plane = st.planes.createPlane(team, type, x, y, a);
+			st.planes.planes.push(plane);
+		}
+	},
+	createPlane: function(team, type, x, y, a) {
+		var plane = {
+			team: team,
+			type: type,			
+			x: x, 
+			y: y,
+			a: a
+		};
+		return plane;
+	}
+};
